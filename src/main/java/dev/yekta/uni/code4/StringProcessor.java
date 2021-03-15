ef4383e6077d77747f998c923957d4e4d58b83a7
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-03-15 12:16:13 +0330

    Add code 4

---
+++ src/main/java/dev/yekta/uni/code4/StringProcessor.java
@@ -0,0 +1,79 @@
+package dev.yekta.uni.code4;
+
+public class StringProcessor {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public StringProcessor(String text) {
+        this.text = text;
+    }
+
+    public StringProcessor reverse() {
+        if (text == null || text.length() < 2)
+            return this;
+
+        char[] textChars = text.toCharArray();
+        int textLength = textChars.length;
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < textLength; i++) {
+            result.append(textChars[textLength - i - 1]);
+        }
+
+        text = result.toString();
+
+        return this;
+    }
+
+    public StringProcessor upperLowerCase() {
+        if (text == null)
+            return this;
+
+        char[] textChars = text.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : textChars) {
+            if (Character.isUpperCase(ch)) {
+                result.append(Character.toLowerCase(ch));
+            } else {
+                result.append(Character.toUpperCase(ch));
+            }
+        }
+
+        text = result.toString();
+
+        return this;
+    }
+
+    public int freq(String str, boolean overlapping) {
+        if (text == null)
+            return 0;
+
+        int textLength = text.length();
+        int strLength = str.length();
+
+        int freq = 0;
+
+        for (int i = 0; i < textLength; i++) {
+            if (textLength < i + strLength)
+                break;
+
+            if (text.substring(i, i + strLength).equals(str)) {
+                freq++;
+
+                if (!overlapping)
+                    i += strLength - 1; // DOES NOT ALLOW OVERLAPPING
+            }
+        }
+
+        return freq;
+    }
+}
---
+++ src/main/java/dev/yekta/uni/code4/StringProcessorTest.java
@@ -0,0 +1,24 @@
+package dev.yekta.uni.code4;
+
+public class StringProcessorTest {
+
+    public static void main(String[] args) {
+        printTestResultOf("Ali Yekta Ali", "Ali");
+        printTestResultOf("Hello World!", "ll");
+        printTestResultOf("aaaaaaaaa", "aa");
+    }
+
+    public static void printTestResultOf(String str, String subStr) {
+        String reverse = new StringProcessor(str).reverse().getText();
+        String upperLowerCase = new StringProcessor(str).upperLowerCase().getText();
+        int freq = new StringProcessor(str).freq(subStr, false);
+        int freqOverlap = new StringProcessor(str).freq(subStr, true);
+
+        System.out.println("============ Test Results ============");
+        System.out.println("Input: " + str);
+        System.out.println("\tReverse: " + reverse);
+        System.out.println("\tUpperLowerCase: " + upperLowerCase);
+        System.out.printf("\tFrequency of '%s': %d\n", subStr, freq);
+        System.out.printf("\tFrequency of '%s' (overlap): %d\n\n", subStr, freqOverlap);
+    }
+}
