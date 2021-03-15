54f97f9d2e0d265408e5a3af6b6bd12fe3a7c4b1
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-03-15 12:16:05 +0330

    Add code 3

---
+++ src/main/java/dev/yekta/uni/code3/Main.java
@@ -0,0 +1,22 @@
+package dev.yekta.uni.code3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(stringFreq("Hello World!", 'l'));
+    }
+
+    public static int stringFreq(String str, char ch) {
+        int freq = 0;
+
+        if (str == null) {
+            return 0;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch)
+                freq++;
+        }
+
+        return freq;
+    }
+}
