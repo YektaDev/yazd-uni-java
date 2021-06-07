256d49c03853a0e2a22f517716c8366a43c052d3
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-06-07 14:22:46 +0430

    Update Finder.java

--- src/main/java/dev/yekta/uni/code13/Finder.java
+++ src/main/java/dev/yekta/uni/code13/Finder.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright © 2021 Ali Khaleqi Yekta, All Rights Reserved.
+ *
+ * Author: Ali Khaleqi Yekta [YektaDev]
+ * Website: https://Yekta.Dev
+ * Email: Me@Yekta.Dev
+ */
+
 package dev.yekta.uni.code13;
 
 import java.util.Scanner;
